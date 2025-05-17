8e454efd9d1251e12f7d6e8b87cdecb56d5c6677
vanss <vans.s3046@gmail.com>
2025-05-17 23:31:46 +0530

    calculator

---
+++ js/calculator.js
@@ -0,0 +1,74 @@
+const keys = [{
+    first : '1',
+    sec : '2',
+    third : '3',
+    fourth : ' + '
+},{
+   first : '4',
+   sec : '5',
+   third : '6',
+   fourth : ' - ' 
+},{
+    first : '7',
+    sec : '8',
+    third : '9',
+    fourth : ' * '
+},{
+    first : '0',
+    sec : '.',
+    third : ' = ',
+    fourth : ' / '
+}];
+
+let keysHTML = '';
+
+keys.forEach((key) => {
+    let html = `
+    <p>
+        <button class="number-button js-button" data-key="${key.first}">
+        ${key.first}</button>
+        <button class="number-button js-button" data-key="${key.sec}">
+        ${key.sec}</button>
+        <button class="number-button js-button" data-key="${key.third}">
+        ${key.third}</button>
+        <button class="operator-button js-button " data-key="${key.fourth}">
+        ${key.fourth}</button>
+    </p>
+    `;
+    keysHTML += html;
+    // console.log(keysHTML);
+});
+document.querySelector('.keys').innerHTML = keysHTML;
+
+document.querySelectorAll('.js-button').forEach( (button) =>{
+    console.log(button);
+    button.addEventListener('click' , () => {
+        const key = button.dataset.key;
+        console.log(key);
+        updatestr(key);
+    });
+});
+
+document.querySelector('.js-clear-button').addEventListener('click',() => {
+    res = '';
+    localStorage.setItem('res',res);
+    localStorage.removeItem(res);
+    document.querySelector('.js-res').innerHTML = `0.00`;
+});
+
+let res= localStorage.getItem('res') || "";
+document.querySelector('.js-res').innerHTML = `${res}`;
+
+function updatestr(c){
+
+    if(c === ' = '){
+        res = eval(res);
+        document.querySelector('.js-res').innerHTML = `${res}`;
+        localStorage.setItem('res',res);
+    }else{
+    res += c;
+    document.querySelector('.js-res').innerHTML = `${res}`;
+    localStorage.setItem('res',res);
+    }
+}
+
